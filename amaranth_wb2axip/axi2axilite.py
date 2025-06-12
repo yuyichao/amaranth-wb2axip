2df06af7002ad8ca2404fa6b6df957da8a261765
Yichao Yu <yyc1992@gmail.com>
2025-06-12 11:17:57 -0400

    Add supports for downsizing axilite

--- amaranth_wb2axip/axi2axilite.py
+++ amaranth_wb2axip/axi2axilite.py
@@ -9,11 +9,14 @@ from .utils import add_verilog_files
 
 
 class AXI2AXILite(wiring.Component):
-    DEPENDENCIES = ['axi2axilite.v', 'skidbuffer.v', 'axi_addr.v', 'sfifo.v']
+    DEPENDENCIES = ['axi2axilsub.v', 'axi2axilite.v', 'skidbuffer.v',
+                    'axi_addr.v', 'sfifo.v']
 
     def __init__(self, data_width, addr_width, id_width, domain='sync', *,
-                 writes=True, reads=True, lowpower=False, lgfifo=4):
+                 sdata_width=None, writes=True, reads=True, lowpower=False, lgfifo=4):
         self.data_width = data_width
+        self.sdata_width = data_width if sdata_width is None else sdata_width
+        assert self.sdata_width <= self.data_width
         self.addr_width = addr_width
         self.id_width = id_width
         self.domain = domain
@@ -22,26 +25,43 @@ class AXI2AXILite(wiring.Component):
         self.lowpower = lowpower
         self.lgfifo = lgfifo
         super().__init__({
-            'axilite': Out(AXI4Lite(data_width, addr_width)),
+            'axilite': Out(AXI4Lite(self.sdata_width, addr_width)),
             'axi': In(AXI4(data_width, addr_width, id_width)),
         })
 
     def elaborate(self, platform):
         m = Module()
-        m.submodules.axi2axil_i = Instance(
-            'axi2axilite',
-            p_C_AXI_ID_WIDTH=self.id_width,
-            p_C_AXI_DATA_WIDTH=self.data_width,
-            p_C_AXI_ADDR_WIDTH=self.addr_width,
-            p_OPT_WRITES=self.writes,
-            p_OPT_READS=self.reads,
-            p_OPT_LOWPOWER=self.lowpower,
-            p_LGFIFO=self.lgfifo,
-            i_S_AXI_ACLK=ClockSignal(self.domain),
-            i_S_AXI_ARESETN=~ResetSignal(self.domain),
-            **self.axi.get_ports_for_instance(prefix='S_AXI_'),
-            **self.axilite.get_ports_for_instance(prefix='M_AXI_'),
-        )
+        if self.sdata_width == self.data_width:
+            m.submodules.axi2axil_i = Instance(
+                'axi2axilite',
+                p_C_AXI_ID_WIDTH=self.id_width,
+                p_C_AXI_DATA_WIDTH=self.data_width,
+                p_C_AXI_ADDR_WIDTH=self.addr_width,
+                p_OPT_WRITES=self.writes,
+                p_OPT_READS=self.reads,
+                p_OPT_LOWPOWER=self.lowpower,
+                p_LGFIFO=self.lgfifo,
+                i_S_AXI_ACLK=ClockSignal(self.domain),
+                i_S_AXI_ARESETN=~ResetSignal(self.domain),
+                **self.axi.get_ports_for_instance(prefix='S_AXI_'),
+                **self.axilite.get_ports_for_instance(prefix='M_AXI_'),
+            )
+        else:
+            m.submodules.axi2axilsub_i = Instance(
+                'axi2axilsub',
+                p_C_AXI_ID_WIDTH=self.id_width,
+                p_C_AXI_M_DATA_WIDTH=self.data_width,
+                p_C_AXI_S_DATA_WIDTH=self.sdata_width,
+                p_C_AXI_ADDR_WIDTH=self.addr_width,
+                p_OPT_WRITES=self.writes,
+                p_OPT_READS=self.reads,
+                p_OPT_LOWPOWER=self.lowpower,
+                p_LGFIFO=self.lgfifo,
+                i_S_AXI_ACLK=ClockSignal(self.domain),
+                i_S_AXI_ARESETN=~ResetSignal(self.domain),
+                **self.axi.get_ports_for_instance(prefix='S_AXI_'),
+                **self.axilite.get_ports_for_instance(prefix='M_AXI_'),
+            )
         add_verilog_files(platform, self.DEPENDENCIES)
         return m
 
